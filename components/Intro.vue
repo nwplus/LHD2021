67bb7aebcd2e295af294a57b17647d359295f368
Michael DeMarco <michaelfromyeg@gmail.com>
2020-08-15 18:17:12 -0600

    Added fancy svg; got it displaying well on desktop

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,31 +1,56 @@
 <template>
-  <div class="intro-section">
-    <p class="intro-header-pre">
-      Welcome to
-    </p>
-    <h1 class="intro-header">
-      Local Hack Day
-    </h1>
-    <p class="intro-p1">
-      UBC Local Hack Day revolves around <b>inclusivity, diversity, and accessibility</b> — we want you to bring your unique perspectives and experiences to the table!
-    </p>
-    <p class="intro-p2">
-      Over the past 3 years, UBC’s Local Hack Day focused on encouraging beginners and people who are curious about technology to work on a project that focuses on these three main pillars.
-    </p>
+  <div class="intro-container">
+    <div class="intro-content">
+      <img :src="introPlank" class="intro-header">
+      <p class="intro-p1">
+        UBC Local Hack Day revolves around <b>inclusivity, diversity, and accessibility</b> — we want you to bring your unique perspectives and experiences to the table!
+      </p>
+      <p class="intro-p2">
+        Over the past 3 years, UBC’s Local Hack Day focused on encouraging beginners and people who are curious about technology to work on a project that focuses on these three main pillars.
+      </p>
+    </div>
+    <img :src="introBg" class="intro-bg">
   </div>
 </template>
 
 <script>
+import introBg from '../assets/sprite/svg/intro__background.svg'
+import introBgMobile from '../assets/sprite/svg/intro__background_m.svg'
+import introPlank from '../assets/sprite/svg/intro__welcome_plank.svg'
+
+export default {
+  props: {},
+  data: function () {
+    return {
+      introBg,
+      introBgMobile,
+      introPlank
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 @import "bulma/bulma.sass";
 
 $body-font: "Source Sans Pro", sans-serif;
+$hero-background-color: #4A414D;
 
 // Desktop CSS
-.intro-section {
-  background-image: url("~@/assets/sprite/svg/intro__background.svg");
+.intro-container {
+  display: flex;
+  justify-content: center;
+  position: relative;
+}
+
+.intro-content {
+  position: absolute;
+  padding-top: 20%;
+}
+
+.intro-bg {
+  width: 100%;
+  background: linear-gradient(180deg, #48414E, 5%, #243745, #566770, #072639);
 }
 
 .intro-header, .intro-p1, .intro-p2 {
